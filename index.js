6720ae03565bc930c182605b645a126f357c993d
vimal <selvamvimaldz1@gmail.com>
2024-01-23 12:57:48 +0530

    fix :add cors origin

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express, { json } from 'express';
 import route from './Router/index.js'
 import cors from 'cors'
 const app=express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors({
+    origin:"https://scribblehere.netlify.app/",
+    methods:["GET","POST","PUT","DELETE"]
+}))
 app.use('/',route);
 app.listen( process.env.PORT,()=>console.log('your app listen '+ process.env.PORT));
