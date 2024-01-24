b114a2f49abe56f89c7a8de9e60f7d0e629259ff
vimal <selvamvimaldz1@gmail.com>
2024-01-24 08:35:40 +0530

    fix: cors

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import route from './Router/index.js'
 import cors from 'cors'
 const app=express();
 app.use(express.json());
-app.use(cors({
-    origin:process.env.ORGIN_URL,
-    methods:["GET","POST","PUT","DELETE"]
-}))
+app.use(cors())
 app.use('/',route);
 app.listen( process.env.PORT,()=>console.log('your app listen '+ process.env.PORT));
