44828c6f31e26e2395633e186f4204ec5dfb9ee6
vimal <selvamvimaldz1@gmail.com>
2024-02-02 23:07:04 +0530

    remove:log

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app=express();
 app.use(express.json());
 app.use(cors())
 app.use('/',route);
-app.listen( process.env.PORT,()=>console.log('your app listen '+ process.env.PORT));
+app.listen( process.env.PORT,()=>console.log('Engine Start '+ process.env.PORT));
